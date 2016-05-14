29605613c2cfba213e8f462c5c9fee07389d2e57
place2work <antimono@yandex.ru>
2016-05-14 21:26:19 +0700

    Добавил метод создания игрового поля и сделал вызов его из конструктора.

--- src/Pole.java
+++ src/Pole.java
@@ -8,6 +8,17 @@ public class Pole {
     public Pole(int pole_size, int lineSize) {
         this.POLE_SIZE = pole_size ;
         this.lineSize = lineSize;
+        createPole();
+    }
+
+    // создание игрового поля
+    public void createPole() {
+        pole = new char[POLE_SIZE][POLE_SIZE];
+        for (int i = 0; i < POLE_SIZE; i++) {
+            for (int j = 0; j < POLE_SIZE; j++) {
+                pole[i][j] = '*';
+            }
+        }
     }
 
     public boolean hasCleanCell() {
