a29415fc93839d62f05ca0d10e05cc20de69b895
place2work <antimono@yandex.ru>
2016-05-14 20:47:38 +0700

    Добавил метод получения символа игрока/компьютера в интерфейс (он понадобится при игре человек-человек и компьютер-компьютер)

--- src/Player.java
+++ src/Player.java
@@ -4,8 +4,10 @@ import java.awt.geom.Point2D;
 public interface Player {
     int getNextX() ;
     int getNextY() ;
+
     Point2d getNextStep() ;
     String getName();
 
+    void setChar(char aChar);
     char getChar();
 }
