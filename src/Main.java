ddb0c16b80847550e783d9ccb01b9acfba64d14b
time2die <time2rfc@gmail.com>
2016-05-24 16:48:18 +0300

    add homework

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import lessons.crossline.CrossLineMain;
+
+import lessons.homework.Task6;
 
 public class Main {
     public static void main(String[] args) {
 //        new CrossLineMain().play();
 //       new  HomeWorkAnswer().work() ;
-        new CunCurExample().example();
+//        new CunCurExample().example();
+       new Task6().start();
     }
 }
---
+++ src/lessons/homework/ReadInput.java
@@ -0,0 +1,20 @@
+package lessons.homework;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class ReadInput {
+    static public List<String> readText() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("text.txt"), StandardCharsets.UTF_8);
+           return lines ;
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+    }
+}
+
