7e6766ce0c4ebd04827421d83282a89e23b56707
place2work <antimono@yandex.ru>
2016-05-26 22:40:37 +0700

    сортировка карты частот слов

--- src/lessons/homework/Task6.java
+++ src/lessons/homework/Task6.java
@@ -36,19 +36,29 @@ public class Task6 {
 
         Set<String> setOne = new HashSet<>(arrayList);// множество значений arrayList для ключей Map
         HashMap<String, Integer> mapFrequency = new HashMap<>();// создать Карту
-        TreeMap<String, Integer> mapFreqSort = new TreeMap<>();// создать Карту
+        TreeMap<Integer, String> mapFreqSort = new TreeMap<>();// создать Карту
 
         // заполнить Карту:
         // ключ Карты - уникальное значение из множетва setOne
         // значение Карты - частота встречаемости каждого ключа данной Карты (значение setOne) в listOne
         for(Iterator<String> iter = setOne.iterator(); iter.hasNext();){// проходимся итератором по множеству setOne
             String val = iter.next();// сохраним очередное значение множества setOne...
+            int freq = Collections.frequency(arrayList, val);// сохраним его частоту
+
             // ... это значение -> ключ к Map;
             // ... частота его в arrayList -> значение Map
-            mapFrequency.put(val, Collections.frequency(arrayList, val));
+            mapFrequency.put(val, freq);
+
+            // Сортировка: если такая частота уже есть, то дописать слово через пробел к уже имеющемуся
+            if(mapFreqSort.containsKey(freq)){
+                val = mapFreqSort.get(freq)+" "+val;
+            }
+
+            // Сортированная по возрастанию ключа (TreeMap) карта частот
+            mapFreqSort.put(freq,val);
         }
 
-        System.out.println(mapFrequency);
+        System.out.println(mapFreqSort);
 
     }
 
