930eae838a6aa1968f42e733bc3bdef7caad1662
place2work <antimono@yandex.ru>
2016-05-27 22:01:48 +0700

    Поменял тип String на ArrayList, теперь нет вероятности потерять данные

--- src/lessons/homework/Task6.java
+++ src/lessons/homework/Task6.java
@@ -19,23 +19,13 @@ public class Task6 {
         List<String> stringList =  new ArrayList<>();
         stringList.addAll(ReadInput.readText());
 
-        //- посчитать частотность повторения всех слов и вывести на экран топ 10
-        String strArray = stringList.toString();// List -> String
-
-        // очистка слов - удаляем "лишние" символы, оставляем все буквы и пробелы
-        String strClear = strArray;
-        strClear = strClear.replaceAll("[^а-яА-Я a-zA-Z]", "");
-
-        // массив слов
-        String[] stringArray =  strClear.trim().split("\\s+");// разбить строку на слова
-
-        //-------------
-        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(stringArray));// String -> List
+        // очистка и разбивка на слова
+        ArrayList<String> arrayList = splitWordList(stringList);
 
         // множество уникальных слов
         Set<String> setOne = new HashSet<>(arrayList);// множество значений arrayList для ключей Map
 
-        HashMap<String, Integer> mapFrequency = new HashMap<>();// создать Карту
+        //HashMap<String, Integer> mapFrequency = new HashMap<>();// создать Карту
         TreeMap<Integer, String> mapFreqSortA = new TreeMap<>();// создать Карту
 
         // заполнить Карту:
@@ -46,7 +36,7 @@ public class Task6 {
             int freq = Collections.frequency(arrayList, val);// сохраним его частоту
 
             //1. уникальное слово - ключ, а его частота - значение
-            mapFrequency.put(val, freq);
+            //mapFrequency.put(val, freq);
 
             // Сортировка: если такая частота уже есть, то дописать слово через пробел к уже имеющемуся
             if(mapFreqSortA.containsKey(freq)){
@@ -75,7 +65,48 @@ public class Task6 {
 
         // количество уникальных слов
         System.out.println("\n" + setOne.size());
+     /*   */
+    }
 
+    /**
+     *  Разбивка на слова и очистка. Возввращает ArrayList<String>
+     * */
+    public ArrayList<String> splitWordList(List<String>stringListIn){
+        ArrayList<String>stringListOut = new ArrayList<>();
+
+        for(int i=0; i < stringListIn.size(); i++) {
+
+            String str = stringListIn.get(i);// строка из коллекции
+            str = str.replaceAll("[^а-яА-Я a-zA-Z]", "");// удаление всех не буквенных символов
+            str = str.toLowerCase();// перевод всех символов в нижний регистр
+            String[] strArr =  str.trim().split("\\s+");// разбить строку на слова по первому пробелу
+            stringListOut.addAll(Arrays.asList(strArr));// упаковать строки в контейнер
+
+        }
+
+        return stringListOut;
+    }
+
+    /**
+     *  Разбивка на слова и очистка. Возввращает StringBuffer
+     * */
+    public StringBuffer splitWord(List<String>stringList){
+        StringBuffer strArr = new StringBuffer();
+
+        strArr.append(stringList);// List -> StringBuffer
+
+        Character ch;
+
+        for(int i=0; i < strArr.length(); i++) {
+            ch = strArr.charAt(i);
+
+            // перевод всех символов в нижний регистр
+            strArr.setCharAt(i,Character.toLowerCase(ch));
+
+            // замена всех небуквенных символов на перенос строки
+            if(!Character.isLetter(ch)) strArr.setCharAt(i,'\n');
+        }
+        return strArr;
     }
 
 }
