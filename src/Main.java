3ff26ce6758e9566be4c9ebc5b01007a1a9ffed3
place2work <antimono@yandex.ru>
2016-05-31 12:24:29 +0700

    изменил под запуск класса с новым заданием

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import lessons.homework.Task6;
+import lessons.homework.Task7;
 
 public class Main {
     public static void main(String[] args) {
 //        new CrossLineMain().play();
 //        new  HomeWorkAnswer().work() ;
 //        new CunCurExample().example();
-        new Task6().start();
+//        new Task6().start();
+        new Task7().start();
     }
 }
