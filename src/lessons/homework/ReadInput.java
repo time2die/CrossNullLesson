0903c2a6c284ed45b3cf926c097ffe76111fa51d
Кем Станислав <mistiman@gmail.com>
2016-06-04 00:14:45 +0300

    изменил ReadInput, список файлов оно теперь возвращает

--- src/lessons/homework/ReadInput.java
+++ src/lessons/homework/ReadInput.java
@@ -1,20 +1,32 @@
 package lessons.homework;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collections;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReadInput {
     static public List<String> readText() {
+//        try {
+//            List<String> lines = Files.readAllLines(Paths.get("text.txt"), StandardCharsets.UTF_8);
+//           return lines ;
+//        } catch (IOException e) {
+//            return Collections.emptyList();
+//        }
+        Stream<Path> pathStream = Stream.empty();
         try {
-            List<String> lines = Files.readAllLines(Paths.get("text.txt"), StandardCharsets.UTF_8);
-           return lines ;
+            pathStream = Files.find(new File("").toPath(), Integer.MAX_VALUE,
+                    (filePath, fileAttr) -> fileAttr.isRegularFile() && filePath.toString().matches(".*.java"));
         } catch (IOException e) {
-            return Collections.emptyList();
+            e.printStackTrace();
         }
+        return pathStream.map(path -> {
+            String[] patha = path.toString().split("\\\\");
+            return patha[patha.length-1];
+        }).collect(Collectors.toList());
     }
 }
 
