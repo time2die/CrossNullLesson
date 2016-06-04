764160d12453d60ecfa1ad2a8ba8da9873691ca1
place2work <antimono@yandex.ru>
2016-06-04 18:31:44 +0700

    ускоряю код

--- src/lessons/homework/Task7.java
+++ src/lessons/homework/Task7.java
@@ -24,25 +24,36 @@ public class Task7 {
         ArrayList<String> wordsList = splitWordList(stringList);
 
         // карта частот слов
-        TreeMap<Integer, String> mapFreqWord = frequencyMapWord(wordsList);
+        //TreeMap<Integer, String> mapFreqWord = frequencyMapWord(wordsList);
+        TreeMap<String, Integer> mapFreqWord = frequencyMapWordA(wordsList);
+
+
+
+        /*
+        TreeMap<Integer, String> sortedMap = new TreeMap(Collections.reverseOrder());
+        for (Map.Entry entry : mapFreqWord.entrySet()) {
+            sortedMap.put((Integer) entry.getValue(), (String)entry.getKey());
+        }
+        */
 
         // разбивка на символы с очисткой от небуквенных символов
-        ArrayList<Character>  charListClear = splitCharList(wordsList);
+        //ArrayList<Character>  charListClear = splitCharList(wordsList);
 
         // карта частот символов
-        TreeMap<Integer, Character> mapFreqCharClear =  frequencyMapChar(charListClear);
-/**/
+        //TreeMap<Integer, Character> mapFreqCharClear =  frequencyMapChar(charListClear);
+
         // выводим на экран первые топ 10 слов
         System.out.println("первые топ 20 слов");
+        TreeMap<Integer, String> sortedMap = reversMap(mapFreqWord);//
         int count = 0;
-        for(Map.Entry e : mapFreqWord.entrySet()){
+        for(Map.Entry e : sortedMap.entrySet()){
             System.out.println(e.getKey()+" "+ e.getValue());
             count++;
             if(count==20){
                 break;
             }
         }
-
+/*
 
         // выводим на экран первые топ 5 очищенных символов
         System.out.println("первые топ 5 символов");
@@ -54,10 +65,11 @@ public class Task7 {
                 break;
             }
         }
-
+        */
         // количество уникальных слов
         Set<String> setOne = new HashSet<>(wordsList);// множество уникальных слов
         System.out.println("\n количество уникальных слов:  " + setOne.size());
+
 //////////
 /*
         myInterface myIfc;
@@ -69,6 +81,85 @@ public class Task7 {
         double a = myIfc.myMeth();
         */
 
+
+
+        //ArrayList <String> arrayListSort = frequencyMapWordA(wordsList);
+
+        //int firstInx = arrayListSort.indexOf(arrayListSort.get(0));
+        //int lastInx = arrayListSort.lastIndexOf(arrayListSort.get(0));
+
+        //int i=0;
+
+        //TreeSet<String> setOne = new TreeSet<>(arrayListSort);// множество значений arrayList для ключей Map
+/*
+        TreeSet<String> sortSet = new TreeSet<>();
+        //TreeMap<Integer, String> mapFreqSort = new TreeMap();
+        TreeMap<String, Integer> mapFreqSort = new TreeMap();
+        int freq1 = 1;
+        int iAdd = 1;
+        String str ="";
+        int count = 0;
+        int freq;
+
+        for(int i=0;i<wordsList.size();i++) {
+            boolean isAdd = sortSet.add(wordsList.get(i));
+
+            //System.out.println(str);
+            if(!isAdd){// если такое слово уже есть
+                freq1++;// счетчик частоты слова увеличиваем
+                mapFreqSort.put(str, freq1);// запишем в карту новую частоту
+                System.out.println(str+" "+freq1);
+            }else {// если такого слова еще нет
+                freq1 = 1;// счетчик частоты слова сбрасываем
+                count++;// счетчик новых слов увеличиваем
+                iAdd = i;
+                str = wordsList.get(i);// запомним новое слово
+                mapFreqSort.put(str, freq1);// запишем в карту новое слово
+                System.out.println(str+" "+freq1);
+            }
+            */
+
+/*
+            if(isAdd){
+
+
+
+                //mapFreqSort.put(count,arrayListSort.get(i));
+                //mapFreqSort.put(arrayListSort.get(i),count);
+
+
+
+            }else{
+                mapFreqSort.put(arrayListSort.get(iAdd),freq1);
+                System.out.println(mapFreqSort.get(arrayListSort.get(iAdd))+" "+freq1);
+                System.out.println(mapFreqSort.get(arrayListSort.get(iAdd))+" "+freq1);
+            }
+            */
+
+        //while (true){
+/*
+            firstInx = arrayListSort.indexOf(arrayListSort.get(i));
+            lastInx = arrayListSort.lastIndexOf(arrayListSort.get(i));
+            */
+            /*
+            firstInx = arrayListSort.indexOf(arrayListSort.get(0));
+            lastInx = arrayListSort.lastIndexOf(arrayListSort.get(0));
+
+            //String str = arrayListSort.get(i);
+            //Collections.replaceAll(arrayListSort, str);
+            arrayListSort.removeAll(arrayListSort.subList(firstInx,lastInx));
+            freq = (lastInx - firstInx) + 1;
+            count++;
+            System.out.println(count);
+            i = lastInx+1;
+            //if (i == arrayListSort.size()) break;
+            if(arrayListSort.isEmpty()) break;
+            */
+
+        //}
+        //System.out.println(count);
+        //System.out.println(firstInx+" "+lastInx+" "+freq);
+
 ///////////////
     }
 
@@ -77,6 +168,52 @@ public class Task7 {
         double myMeth();
     }
     */
+
+
+    /**
+     * Меняет местами ключ и значение а так же сортирует по убыванию
+     * */
+    public TreeMap<Integer, String> reversMap(TreeMap<String, Integer> treeMap){
+        TreeMap<Integer, String> flipMap = new TreeMap(Collections.reverseOrder());
+        for (Map.Entry entry : treeMap.entrySet()) {
+            flipMap.put((Integer) entry.getValue(), (String)entry.getKey());
+        }
+
+        return flipMap;
+    }
+
+    /**
+     * Карта частот слов сортированная по возрастанию
+     * */
+
+    public TreeMap<String, Integer> frequencyMapWordA(ArrayList<String>arrayList){
+
+        Collections.sort(arrayList);
+
+        TreeSet<String> sortSet = new TreeSet<>();
+        TreeMap<String, Integer> mapFreqSort = new TreeMap<>();
+
+        int freq = 1;
+        String word ="";
+
+        for(int i=0;i<arrayList.size();i++) {
+            boolean isAdd = sortSet.add(arrayList.get(i));
+
+            if (!isAdd) {// если такое слово уже есть
+                freq++;// счетчик частоты слова увеличиваем
+                mapFreqSort.put(word, freq);// запишем в карту новую частоту
+
+            } else {// если такого слова еще нет
+                freq = 1;// счетчик частоты слова сбрасываем
+                word = arrayList.get(i);// запомним новое слово
+                mapFreqSort.put(word, freq);// запишем в карту новое слово
+
+            }
+        }
+
+        return mapFreqSort;
+    }
+
     /**
      *  Разбивка на символы
      * */
@@ -124,6 +261,7 @@ public class Task7 {
 
         // заполнить Карту:
         for(String setIter : setOne){// для каждой строки из множества setOne
+            /**/
             int freq = Collections.frequency(arrayList, setIter);// сохраним его частоту
 
             // Для сортировки - если такая частота уже есть, то дописать слово через пробел к уже имеющемуся
@@ -133,10 +271,14 @@ public class Task7 {
             }
             // Сортировка частот слов по убыванию ключа
             mapFreqSortA.put(freq,setIter);
+
+            //arrayList.remove(setIter);
+
         }
 
         return mapFreqSortA;
     }
+
 
     /**
      * Создание частотной карты символов
