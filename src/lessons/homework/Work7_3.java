2ed50aa1fee37cb1cc5668374d14658e516accfc
place2work <antimono@yandex.ru>
2016-06-05 23:41:56 +0700

    Удалил интерфейс и добавил класс

---
+++ src/lessons/homework/Work7_3.java
@@ -0,0 +1,7 @@
+package lessons.homework;
+
+/**
+ * Created by place2work on 05.06.2016.
+ */
+public class Work7_3 {
+}
--- src/lessons/homework/Work7_interface.java
+++
@@ -1,9 +0,0 @@
-package lessons.homework;
-
-/**
- * Created by place2work on 04.06.2016.
- */
-
-public interface Work7_interface {
-    double myMeth();
-}
