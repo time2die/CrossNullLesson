d6f2461407241fd510762177ade0d9d2487fd7b6
place2work <antimono@yandex.ru>
2016-06-10 21:34:13 +0700

    Добавил ф-ию чтения файлов из папки проекта

--- src/lessons/homework/ReadInput.java
+++ src/lessons/homework/ReadInput.java
@@ -1,20 +1,61 @@
 package lessons.homework;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReadInput {
+    /**
+     * Чтение файла
+     * */
     static public List<String> readText() {
         try {
-            List<String> lines = Files.readAllLines(Paths.get("text.txt"), StandardCharsets.UTF_8);
+
+            //List<String> lines = Files.readAllLines(Paths.get("text.txt"), StandardCharsets.UTF_8);
+
+            // То же самое, но через Стримы и лямбды
+            List<String> lines = Files.lines(Paths.get("text.txt"), StandardCharsets.UTF_8)
+                    .collect(Collectors.toList());
+
             return lines;
         } catch (IOException e) {
             return Collections.emptyList();
         }
     }
+
+    /**
+     * Чтения файлов из заданной папки
+     * */
+    static public void readTextFile() {
+        try {
+            Charset charset = Charset.forName("UTF-8");
+            String myDir = "./src/lessons/homework";
+
+            File folder = new File(myDir);
+            if (folder.isDirectory()) {
+                for (File f : folder.listFiles()) {
+                    Path myPath = Paths.get(myDir,f.getName());
+                    List<String> lines = Files.readAllLines(myPath, charset);
+                    for (String line : lines) {
+                        System.out.println(line);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
 
